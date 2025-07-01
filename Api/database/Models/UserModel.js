751c10cd82795d422857a03be5fbaf3e58b2497d
Eyüp Öztürk <eyupoztrk04@gmail.com>
2025-07-02 01:40:43 +0300

    #6 auth route created

--- Api/database/Models/UserModel.js
+++ Api/database/Models/UserModel.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require('bcrypt');
+const Enum = require("../../config/Enum");
+const CustomError = require("../../lib/Error");
 
 
 const userSchema = new mongoose.Schema({
@@ -11,7 +15,46 @@ const userSchema = new mongoose.Schema({
         timestamps: true
     });
 
-class UserModel extends mongoose.model {
+class UserModel extends mongoose.Model {
+
+
+    validPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+
+    static hashPassword(password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    }
+
+    static async checkUserBeforeRegister(email, username) {
+
+        let checkEmail = await this.findOne({ email });
+        let checkUsername = await this.findOne({ username });
+
+        if (checkEmail)
+            throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, Enum.RESPONSE_MESSAGES.BAD_REQUEST, "Email is already used");
+        if (checkUsername)
+            throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, Enum.RESPONSE_MESSAGES.BAD_REQUEST, "Username is already used");
+
+        return null;
+    }
+
+    static checkBody(body) {
+        if (!body.email)
+            throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, Enum.RESPONSE_MESSAGES.BAD_REQUEST, "Email field is required");
+        if (!validator.isEmail(body.email))
+            throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, Enum.RESPONSE_MESSAGES.BAD_REQUEST, "Email format is not valid");
+        if (!body.password)
+            throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, Enum.RESPONSE_MESSAGES.BAD_REQUEST, "Password field is required");
+
+        if (typeof body.password !== "string" || body.password.length < 7)
+            throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "email or password wrong");
+
+        return null;
+    }
+
+   
+
 
 }
 
---
+++ Api/lib/auth.js
@@ -0,0 +1,55 @@
+
+const passport = require('passport');
+const { ExtractJwt, Strategy } = require('passport-jwt');
+const config = require("../config");
+const userModel = require("../database/Models/UserModel");
+
+
+
+module.exports = function () {
+    
+    let strategy = new Strategy({
+        secretOrKey: config.JWT.SECRET,
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+         
+    },
+        async (payload, done) => {
+            try {
+
+                let user = await userModel.findOne({ _id: payload.id });
+
+                if (user) {
+
+                    done(null, {
+                        id: user._id,
+                        username: user.username,
+                        email: user.email,
+                        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24)
+                    });
+                } else {
+
+                    done(new Error("user not fount"), null);
+                }
+            }
+            catch (err) {
+
+                done(err, null);
+            }
+
+        }
+    );
+
+
+    passport.use(strategy);
+
+    return {
+        initialize: function () {
+            return passport.initialize();
+        },
+        authenticate: function () {
+            return passport.authenticate("jwt", { session: false });
+        }
+
+
+    }
+}
